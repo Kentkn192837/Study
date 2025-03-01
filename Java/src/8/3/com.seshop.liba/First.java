af83fcf1c051e0bda1ca9f3f6563e01cd47ed943
Kentkn192837 <agriculture5012@gmail.com>
2025-03-02 03:12:18 +0900

    packageで依存関係のあるプログラムを編集

---
+++ Java/src/8/3/com.seshop.liba/First.java
@@ -0,0 +1,10 @@
+package com.seshop.sample.firstpkg;
+
+import com.seshop.sample.firstpkg.Third;
+
+public class First {
+    public static void x() {
+        System.out.println("First::x");
+        Third.z();
+    }
+}
