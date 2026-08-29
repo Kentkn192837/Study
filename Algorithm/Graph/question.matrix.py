f3103bb5d664878e2460eda7857c3a42cc5cb0fa
Kentkn192837 <agriculture5012@gmail.com>
2026-08-29 16:24:42 +0900

    探索候補の座標が範囲内かどうかのチェック

--- Algorithm/Graph/question.matrix.py
+++ Algorithm/Graph/question.matrix.py
@@ -12,6 +12,7 @@ que = deque([(0, 0, 0)])
 visited = set([])
 dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
+H, W = len(matrix), len(matrix[0])
 
 while que:
     current_y, current_x, d = que.popleft()
@@ -21,4 +22,12 @@ while que:
     for i in range(4):
         next_y = current_y + dy[i]
         next_x = current_x + dx[i]
+        if next_y < 0 or next_y >= H:
+            continue
+        if next_x < 0 or next_x >= W:
+            continue
+        if matrix[next_y][next_x] == 1:
+            continue
+        if (next_y, next_x) in visited:
+            continue
         que.append((next_y, next_x, d + 1))
