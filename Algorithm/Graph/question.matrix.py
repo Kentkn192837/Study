3911fbdcb0e50fc9fa7446925b9d5bdcfa9539eb
Kentkn192837 <agriculture5012@gmail.com>
2026-08-29 16:19:35 +0900

    次に探索するノードの座標をキューに入れる

--- Algorithm/Graph/question.matrix.py
+++ Algorithm/Graph/question.matrix.py
@@ -10,9 +10,15 @@ matrix = [
 
 que = deque([(0, 0, 0)])
 visited = set([])
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
 
 while que:
     current_y, current_x, d = que.popleft()
     if (current_y, current_x) in visited:
         continue
     visited.add((current_y, current_x))
+    for i in range(4):
+        next_y = current_y + dy[i]
+        next_x = current_x + dx[i]
+        que.append((next_y, next_x, d + 1))
