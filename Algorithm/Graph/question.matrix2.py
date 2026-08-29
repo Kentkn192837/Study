03de54d1383431860bc93814e5f58d8b95d9adcc
Kentkn192837 <agriculture5012@gmail.com>
2026-08-29 22:39:09 +0900

    各セルをノードとみなす

--- Algorithm/Graph/question.matrix2.py
+++ Algorithm/Graph/question.matrix2.py
@@ -7,3 +7,14 @@ matrix = [
     [1, 0, 1, 0, 1, 0, 1],
     [1, 1, 0, 1, 1, 1, 1],
 ]
+
+H, W = len(matrix), len(matrix[0])
+N = H * W
+visited = [False for _ in range(N)]
+
+for i in range(N):
+    que = deque([i])
+    while que:
+        current = que.popleft()
+        if visited[current]:
+            continue
