e3fe8da20a9ea87913699607960615ff3d4906e3
Kentkn192837 <agriculture5012@gmail.com>
2026-08-29 22:52:41 +0900

    探索済みのセルと値が1のセルはスキップする

--- Algorithm/Graph/question.matrix2.py
+++ Algorithm/Graph/question.matrix2.py
@@ -13,8 +13,13 @@ N = H * W
 visited = [False for _ in range(N)]
 
 for i in range(N):
+    current_y, current_x = i // W, i % W
+    if visited[i] or matrix[current_y][current_x] == 1:
+        continue
+
     que = deque([i])
     while que:
         current = que.popleft()
         if visited[current]:
             continue
+        visited[current] = True
